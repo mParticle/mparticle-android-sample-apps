ff9c0a1d89a81377930677df83c438e8346ff1f3
Felipe Coronado <f.coronado838@gmail.com>
2022-08-31 14:16:49 -0500

    Updated Advertising ID permissions on manifest, updated settings.gradle.kts, and build.gradle.kts to latest dependencies and SDK 33.

--- core-sdk-samples/higgs-shop-sample-app/app/build.gradle.kts
+++ core-sdk-samples/higgs-shop-sample-app/app/build.gradle.kts
@@ -4,17 +4,17 @@ import java.util.TimeZone
 
 plugins {
     id("com.android.application")
-    id("org.jlleitschuh.gradle.ktlint") version "10.2.1"
+    id("org.jlleitschuh.gradle.ktlint") version "11.0.0"
     kotlin("android")
     kotlin("kapt")
 }
 
 android {
-    compileSdk = 31
+    compileSdk = 33
     defaultConfig {
         applicationId = "com.mparticle.example.higgsshopsampleapp"
         minSdk = 23
-        targetSdk = 31
+        targetSdk = 33
         versionCode = buildVersionCode()
         versionName = "0.11.5-SNAPSHOT"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
@@ -49,7 +49,7 @@ dependencies {
     implementation("androidx.recyclerview:recyclerview:1.2.1")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.3.1")
     implementation("com.google.android.material:material:1.5.0")
-    implementation("com.mparticle:android-core:5.35.3")
+    implementation("com.mparticle:android-core:5.44.2")
 
     implementation("androidx.navigation:navigation-fragment:2.3.5")
     implementation("androidx.navigation:navigation-ui:2.3.5")
@@ -57,11 +57,11 @@ dependencies {
     implementation("androidx.navigation:navigation-fragment-ktx:2.3.5")
     implementation("androidx.navigation:navigation-ui-ktx:2.3.5")
 
-    implementation("com.github.bumptech.glide:glide:4.11.0")
+    implementation("com.github.bumptech.glide:glide:4.13.2")
 
-    api("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.3.7")
-    api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.0")
-    api("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.4.1")
+    api("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.6.4")
+    api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+    api("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
 
     implementation("com.squareup.retrofit2:retrofit:2.6.1")
     implementation("com.squareup.retrofit2:converter-gson:2.6.1")
@@ -69,9 +69,9 @@ dependencies {
     implementation("com.squareup.retrofit2:adapter-rxjava2:2.6.1")
     implementation("io.reactivex.rxjava2:rxjava:2.1.14")
     implementation("io.reactivex.rxjava2:rxandroid:2.0.2")
-    implementation("com.squareup.okhttp3:logging-interceptor:3.12.12")
-    implementation("com.squareup.okhttp3:okhttp:3.12.12")
-    implementation("com.squareup.okhttp3:logging-interceptor:3.12.12")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
+    implementation("com.squareup.okhttp3:okhttp:4.10.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.3.1")
     implementation("androidx.navigation:navigation-fragment-ktx:2.3.5")
     implementation("androidx.navigation:navigation-ui-ktx:2.3.5")
@@ -99,7 +99,7 @@ dependencies {
     androidTestImplementation("androidx.test.ext:truth:1.4.0")
     androidTestImplementation("com.google.truth:truth:1.1.3")
     androidTestImplementation("androidx.test:runner:1.4.0")
-    androidTestImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.0")
+    androidTestImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.4")
     androidTestUtil("androidx.test:orchestrator:1.4.1")
 }
 
