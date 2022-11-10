a8f0507d07567ffdb9290bd10f94b0dd54cc9208
markvdouw <v-mvanderouw@mparticle.com>
2022-11-10 13:00:40 -0300

    Adding module for internal testing

--- core-sdk-samples/higgs-shop-sample-app/app/src/androidTest/kotlin/com/mparticle/example/higgsshopsampleapp/TestExtFunctions.kt
+++ core-sdk-samples/higgs-shop-sample-app/app/src/androidTest/kotlin/com/mparticle/example/higgsshopsampleapp/TestExtFunctions.kt
@@ -5,6 +5,9 @@ import androidx.lifecycle.Observer
 import kotlin.coroutines.resume
 import kotlin.coroutines.suspendCoroutine
 
+/**
+ * Get value from LiveData using suspend coroutine
+ */
 suspend fun <T> LiveData<T?>.getValueFromLiveData() = suspendCoroutine<T?> {
     var observer: Observer<T?>? = null
     observer = Observer<T?> { t ->
