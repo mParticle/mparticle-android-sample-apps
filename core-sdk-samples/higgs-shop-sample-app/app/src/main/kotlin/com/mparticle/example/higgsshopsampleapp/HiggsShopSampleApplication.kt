83f6fd379f84f3bf4fe0b4a8faea5e60c2435828
markvdouw <v-mvanderouw@mparticle.com>
2023-06-15 20:43:33 -0300

    Sideloaded kit integration

--- core-sdk-samples/higgs-shop-sample-app/app/src/main/kotlin/com/mparticle/example/higgsshopsampleapp/HiggsShopSampleApplication.kt
+++ core-sdk-samples/higgs-shop-sample-app/app/src/main/kotlin/com/mparticle/example/higgsshopsampleapp/HiggsShopSampleApplication.kt
@@ -3,6 +3,7 @@ package com.mparticle.example.higgsshopsampleapp;
 import android.app.Application
 import com.mparticle.MParticle
 import com.mparticle.MParticleOptions
+import com.mparticle.kits.LocalKit
 import com.mparticle.networking.NetworkOptions
 
 class HiggsShopSampleApplication: Application() {
@@ -10,8 +11,10 @@ class HiggsShopSampleApplication: Application() {
     override fun onCreate() {
         super.onCreate()
         val options: MParticleOptions = MParticleOptions.builder(this)
-            .credentials(BuildConfig.HIGGS_SHOP_SAMPLE_APP_KEY, BuildConfig.HIGGS_SHOP_SAMPLE_APP_SECRET)
+            .credentials("us1-00d2e177de62f84baae3239825ced69b", "WVqu6dsZ9YGxcLNFsfB97tfp8Ov2uMXHYXFMLjryXfx5iDap1RWIFWLkgAAUOenC")//BuildConfig.HIGGS_SHOP_SAMPLE_APP_KEY, BuildConfig.HIGGS_SHOP_SAMPLE_APP_SECRET)
             .environment(MParticle.Environment.Development)
+            .sideloadedKits(mutableListOf(LocalKit().apply { kit = LoggingCustomKit() }) as List<Any>)
+//            .addCustomKit(7829, LoggingCustomKit() )
             // Disable SSL pinning for debugging network requests
 //            .networkOptions(
 //                NetworkOptions.builder()
---
+++ core-sdk-samples/higgs-shop-sample-app/app/src/main/kotlin/com/mparticle/example/higgsshopsampleapp/LoggingCustomKit.kt
@@ -0,0 +1,87 @@
+package com.mparticle.example.higgsshopsampleapp
+
+import android.content.Context
+import android.util.Log
+import com.mparticle.MPEvent
+import com.mparticle.internal.CoreCallbacks.KitListener
+import com.mparticle.kits.KitIntegration
+import com.mparticle.kits.KitIntegration.EventListener
+import com.mparticle.kits.ReportingMessage
+import java.lang.Exception
+
+class LoggingCustomKit() : KitIntegration(), KitListener, EventListener {
+
+    companion object {
+        private const val CUSTOM_KIT = "Custom Kit"
+    }
+
+    override fun getName(): String = CUSTOM_KIT
+
+    override fun onKitCreate(
+        settings: MutableMap<String, String>?,
+        context: Context?
+    ): MutableList<ReportingMessage> {
+        Log.d(CUSTOM_KIT, "CUSTOM_KIT onKitCreate")
+        return mutableListOf()
+    }
+
+    override fun leaveBreadcrumb(breadcrumb: String?): MutableList<ReportingMessage> {
+        Log.d(CUSTOM_KIT, "CUSTOM_KIT event log with name ")
+        return mutableListOf()
+    }
+
+    override fun logError(
+        message: String?,
+        errorAttributes: MutableMap<String, String>?
+    ): MutableList<ReportingMessage> {
+        Log.d(CUSTOM_KIT, "CUSTOM_KIT event log with name ")
+        return mutableListOf()
+    }
+
+    override fun logException(
+        exception: Exception?,
+        exceptionAttributes: MutableMap<String, String>?,
+        message: String?
+    ): MutableList<ReportingMessage> {
+        Log.d(CUSTOM_KIT, "CUSTOM_KIT event log with name ")
+        return mutableListOf()
+    }
+
+    override fun logEvent(baseEvent: MPEvent): MutableList<ReportingMessage>? {
+        Log.d(CUSTOM_KIT, "CUSTOM_KIT event log with name ${baseEvent.eventName}")
+        return super.logEvent(baseEvent)
+    }
+
+    override fun logScreen(
+        screenName: String?,
+        screenAttributes: MutableMap<String, String>?
+    ): MutableList<ReportingMessage> {
+        Log.d(CUSTOM_KIT, "CUSTOM_KIT event log with name ")
+        return mutableListOf()
+    }
+
+    override fun setOptOut(optedOut: Boolean): MutableList<ReportingMessage> = mutableListOf()
+    override fun kitFound(p0: Int) {
+        Log.d(CUSTOM_KIT, "CUSTOM_KIT event log with name ")
+    }
+
+    override fun kitConfigReceived(p0: Int, p1: String?) {
+        Log.d(CUSTOM_KIT, "CUSTOM_KIT event log with name ")
+    }
+
+    override fun kitExcluded(p0: Int, p1: String?) {
+        Log.d(CUSTOM_KIT, "CUSTOM_KIT event log with name ")
+    }
+
+    override fun kitStarted(p0: Int) {
+        Log.d(CUSTOM_KIT, "CUSTOM_KIT event log with name ")
+    }
+
+    override fun onKitApiCalled(p0: Int, p1: Boolean?, vararg p2: Any?) {
+        Log.d(CUSTOM_KIT, "CUSTOM_KIT event log with name ")
+    }
+
+    override fun onKitApiCalled(p0: String?, p1: Int, p2: Boolean?, vararg p3: Any?) {
+        Log.d(CUSTOM_KIT, "CUSTOM_KIT event log with name ")
+    }
+}
