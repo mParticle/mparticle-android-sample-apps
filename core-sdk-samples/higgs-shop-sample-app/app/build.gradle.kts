73534a1c6c347546f31697c294ee6d17cc4796c2
James Newman <james.newman@rokt.com>
2025-07-11 22:16:12 +1000

    Add lint baseline

--- core-sdk-samples/higgs-shop-sample-app/app/build.gradle.kts
+++ core-sdk-samples/higgs-shop-sample-app/app/build.gradle.kts
@@ -52,6 +52,10 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
+    lint {
+        baseline = file("lint-baseline.xml")
+        disable += "MParticleVersionInconsistency"
+    }
 }
 
 dependencies {
